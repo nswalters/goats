ffe53b7df301511befd81d34347ce4fbc2f19245
Nick Walters <nswalters.s@gmail.com>
2020-08-10 20:44:00 -0500

    Fixed grammar

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -9,7 +9,7 @@ class Goat extends React.Component {
         <img className='card-img-top' src={goat.imgUrl} alt='one specific goat' />
         <div className='card-body'>
           <h5 className="card-title">{goat.name}</h5>
-          <p className="card-text">This goat is {goat.age} years old and are described as: {goat.description}</p>
+          <p className="card-text">This goat is {goat.age} years old and is described as: {goat.description}</p>
         </div>
       </div>
     );
